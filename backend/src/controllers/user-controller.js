144f2b40d6bdd95341eabd75148ed96c01e931a5
Jia Qing <jiaqing2303@gmail.com>
2021-03-03 21:13:59 +0800

    Restructure and add example

---
+++ backend/src/controllers/user-controller.js
@@ -0,0 +1,89 @@
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+module.exports["createUser"] = async function(req, res) {
+   try {
+       const { displayName, password, email, role } = req.body
+
+       if (!displayName || !password || !email || !role) {
+           return res.status(400).send({ message: 'Missing fields' })
+       }
+
+       const { uid } = await admin.auth().createUser({
+           displayName,
+           password,
+           email
+       })
+       await admin.auth().setCustomUserClaims(uid, { role })
+
+       return res.status(201).send({ uid })
+   } catch (err) {
+       return handleError(res, err)
+   }
+}
+
+function handleError(res, err) {
+   return res.status(500).send({ message: `${err.code} - ${err.message}` });
+}
+
+module.exports["getAllUsers"] = async function(req, res) {
+    try {
+        const listUsers = await admin.auth().listUsers()
+        const users = listUsers.users.map(mapUser)
+        return res.status(200).send({ users })
+    } catch (err) {
+        return handleError(res, err)
+    }
+}
+
+function mapUser(user) {
+    const customClaims = (user.customClaims || { role: '' }) 
+    const role = customClaims.role ? customClaims.role : ''
+    return {
+        uid: user.uid,
+        email: user.email || '',
+        displayName: user.displayName || '',
+        role,
+        lastSignInTime: user.metadata.lastSignInTime,
+        creationTime: user.metadata.creationTime
+    }
+}
+
+module.exports["getUser"] = async function(req, res) {
+   try {
+       const { id } = req.params
+       const user = await admin.auth().getUser(id)
+       return res.status(200).send({ user: mapUser(user) })
+   } catch (err) {
+       return handleError(res, err)
+   }
+}
+
+module.exports["updateUser"] = async function(req, res) {
+   try {
+       const { id } = req.params
+       const { displayName, password, email, role } = req.body
+
+       if (!id || !displayName || !password || !email || !role) {
+           return res.status(400).send({ message: 'Missing fields' })
+       }
+
+       await admin.auth().updateUser(id, { displayName, password, email })
+       await admin.auth().setCustomUserClaims(id, { role })
+       const user = await admin.auth().getUser(id)
+
+       return res.status(204).send({ user: mapUser(user) })
+   } catch (err) {
+       return handleError(res, err)
+   }
+}
+
+module.exports["removeUser"] = async function(req, res) {
+   try {
+       const { id } = req.params
+       await admin.auth().deleteUser(id)
+       return res.status(204).send({})
+   } catch (err) {
+       return handleError(res, err)
+   }
+}
