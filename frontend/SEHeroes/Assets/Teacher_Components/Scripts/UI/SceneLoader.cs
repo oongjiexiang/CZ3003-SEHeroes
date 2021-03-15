8a0536248d6f6e54b2f2c250412753673e1cc2e0
Bryan Lim <blim0901@gmail.com>
2021-03-15 22:27:10 +0800

    Added the table in teacher's mode tutorial group UI to display the list of tutorial groups that the teacher manages.

---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene() 
+    {
+    	int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    	SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadHomeScene()
+    {
+    	SceneManager.LoadScene(0);
+    }
+
+    public void LoadTutorialGroupScene()
+    {
+    	SceneManager.LoadScene(1);
+    }
+
+    public void LoadStoryModeScene()
+    {
+    	SceneManager.LoadScene(2);
+    }
+
+    public void LoadQuestionBankScene()
+    {
+    	SceneManager.LoadScene(3);
+    }
+
+    public void LoadLeaderboardScene()
+    {
+    	SceneManager.LoadScene(4);
+    }
+
+    public void LoadAssignmentsScene()
+    {
+    	SceneManager.LoadScene(5);
+    }
+
+    public void LoadSettingsScene()
+    {
+    	SceneManager.LoadScene(6);
+    }
+
+
+}
