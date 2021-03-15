4f6468a318bd4d3773bc5be0e46eecbb6433c1d9
Bryan Lim <blim0901@gmail.com>
2021-03-15 15:01:22 +0800

    added the main user interface scenes for the teacher's mode under Scenes > Teacher UI. Also added scripts to control the swtiching between the teacher scenes under Teacher_Components > Scripts

---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Tab_Button.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+	[RequireComponent(typeof(Image))]
+public class Tab_Button : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private int tabIndex;
+    private Image image;
+
+    private Tab_Controller controller;
+
+    private bool isActive = false;
+
+    private void Awake()
+    {
+    	controller = FindObjectOfType<Tab_Controller>();
+    	image = GetComponent<Image>();
+    }
+
+    public void SetIndex(int _index)
+    {
+    	tabIndex = _index;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+    	controller.ButtonMouseClick(tabIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    	if (!isActive)
+    	{
+    		image.color = controller.mouseEnterColor;
+    	}
+
+    	controller.ButtonMouseEnter(tabIndex);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    	if (!isActive)
+    	{
+    		image.color = controller.normalColor;
+    	}
+
+    	controller.ButtonMouseExit(tabIndex);
+    }
+
+    public void ToggleActive()
+    {
+    	isActive = !isActive;
+
+    	if (isActive) 
+    	{
+    		image.color = controller.mouseClickColor;
+    	}
+
+    	else
+    	{
+    		image.color = controller.normalColor;
+    	}
+
+    }
+
+
+}
---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Tab_Controller.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tab_Controller : MonoBehaviour
+{
+    [Header("Panels")]
+    public Transform buttonsPanel;
+    public Transform contentsPanel;
+
+    [Header("Button State Color")]
+    public Color normalColor;
+    public Color mouseEnterColor;
+    public Color mouseClickColor;
+
+    [Header("Events")]
+    public UnityEvent TabSelectionChangedEvent;
+
+    private int selectedIndex;
+    private Tab_Button selectedButton;
+
+    private List<Tab_Button> buttons = new List<Tab_Button>();
+    private List<Transform> panels = new List<Transform>();
+   
+    private void Start()
+    {
+    	for (int i = 0; i < buttonsPanel.transform.childCount; i++)
+    	{
+    		GameObject buttonGo = buttonsPanel.transform.GetChild(i).gameObject;
+    		Tab_Button button = buttonGo.GetComponent<Tab_Button>();
+    		button.SetIndex(i);
+    		buttons.Add(button);
+    	}
+
+    	foreach (Transform item in contentsPanel.transform)
+    	{
+    		panels.Add(item);
+    	}
+
+		ButtonMouseClick(0);
+    }
+
+    public void ButtonMouseClick(int _id)
+    {
+    	if (selectedButton != null)
+    	{
+    		selectedButton.ToggleActive();
+    	}
+
+    	selectedIndex = _id;
+    	selectedButton = buttons[selectedIndex];
+    	selectedButton.ToggleActive();
+    	HideAllPanels();
+    }
+
+    public void ButtonMouseEnter(int _id)
+    {
+    	Debug.Log("Button Mouse Enter");
+    }
+
+    public void ButtonMouseExit(int _id)
+    {
+    	Debug.Log("Button Mouse Exit");
+    }
+
+    private void HideAllPanels()
+    {
+    	for (int i = 0; i < panels.Count; i++)
+    	{
+    		if(i == selectedIndex)
+    		{
+    			panels[i].gameObject.SetActive(true);
+    		}
+    		else
+    		{
+    			panels[i].gameObject.SetActive(false);
+    		}
+    	}
+
+    	if (TabSelectionChangedEvent != null)
+    	{
+    		TabSelectionChangedEvent.Invoke();
+    	}
+    }
+
+}
