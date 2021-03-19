8333e12f401ec416515d30ea82a8695f674cc19c
YangXingHao96 <60702299+YangXingHao96@users.noreply.github.com>
2021-03-20 01:16:26 +0800

    Add files via upload

---
+++ backend/src/routes/world.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const WorldController = require("../controllers/world-controller");
+
+
+router.patch("/add_update", (req, res) => {
+  const { sectionNo, worldNo, tutorialGroupID,unlockDate } = req.body;
+
+  WorldController.update_add_restrictions(
+    {
+        sectionNo: sectionNo,
+        worldNo: worldNo,
+        tutorialGroupID: tutorialGroupID,
+        unlockDate: new Date(unlockDate)
+    },
+
+    (err, msg) => {
+      if (err) {
+        return res.status(500).send({ message: `${err}` });
+      } else {
+        return res.status(200).send(msg);
+      }
+    }
+  );
+});
+
+router.delete("/", (req, res) => {
+  const { sectionNo, worldNo, tutorialGroupID, unlockDate } = req.body;
+
+  WorldController.remove_restriction(
+    {
+      sectionNo: sectionNo,
+      worldNo: worldNo,
+      tutorialGroupID: tutorialGroupID,
+    },
+
+    (err, msg) => {
+      if (err) {
+        return res.status(500).send({ message: `${err}` });
+      } else {
+        return res.status(200).send(msg);
+      }
+    }
+  );
+});
+
+module.exports = router;
