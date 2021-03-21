3e70cb6cd149ea423f2068644add0692d3bbf33a
yuanharnteo <bryson130191@gmail.com>
2021-03-21 10:32:26 +0800

    world-selection, 5worlds, level-selection initial push

---
+++ frontend/SEHeroes/Assets/Scripts/LevelSelectionController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionController : MonoBehaviour
+{
+    public string level;
+    SpriteRenderer renderer;
+    Color initialColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>(); 
+        initialColor = renderer.material.color;
+        // initialColor.a = 0.7f;
+        // renderer.material.color = initialColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        switch(level) {
+            case "Easy":
+                SceneManager.LoadScene(sceneName:"Forest");
+                break;
+            case "Medium":
+                SceneManager.LoadScene(sceneName:"Village");
+                break;
+            case "Hard":
+                SceneManager.LoadScene(sceneName:"Snowland");
+                break;
+        }
+    }
+
+    void OnMouseOver() {
+        initialColor.a = 0.7f;
+        renderer.material.color = initialColor;
+
+    }
+
+    void OnMouseExit() {
+        initialColor.a = 1.0f;
+        renderer.material.color = initialColor;
+    }
+}
