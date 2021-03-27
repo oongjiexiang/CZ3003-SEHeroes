97d49ee60cd6a0bec473bce5941d4099f451b560
Bryan Lim <blim0901@gmail.com>
2021-03-28 00:31:12 +0800

    Added Tutorial group API controller to get data and display tutorial data

--- frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Tutorial_List_Script.cs
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Tutorial_List_Script.cs
@@ -3,176 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-
-/*
-public class Tutorial_List_Script : MonoBehaviour
-{
-    List<string> tutorialList;
-    List<string> studentNumberList;
-    GameObject tutorialPopUp;
-    public Text noRecordLabel;
-    public GameObject mainScrollContentView;
-    public GameObject ContentDataPanel;
-
-    //use this for initialization
-    void Start () 
-    {
-       setTutorialListData ();
-       setStudentsNumberListData();
-       tutorialPopupInitialized ();
-    }
-
-    //Update is called once per frame
-    void Update ()
-    {
-
-    }
-
-    //public void backButtonAction
-    //{
-
-    //}
-
-    //public void computerButtonAction
-    //{
-        //manager.Instance.isComputerVsPlayer = true;
-        //SceneManager.LoadScene (2);
-    //}
-
-
-    //Tutorial popup methods
-    void setTutorialListData() 
-    {
-        tutorialList = new List<string> ();
-        int index;
-        for (int i = 0; i < 15; i++)
-        {
-            index = Random.Range(0, 10000);
-            tutorialList.Add (index.ToString());
-        }
-    }
-
-    void setStudentsNumberListData() 
-    {
-        studentNumberList = new List<string> ();
-        int index;
-        for (int i = 0; i < 15; i++)
-        {
-            index = Random.Range(0, 100);
-            studentNumberList.Add (index.ToString());
-        }
-    }
-
-    void tutorialPopupInitialized () 
-    {
-        if (tutorialList.Count > 0)
-        {
-            noRecordLabel.gameObject.SetActive (false);
-            RectTransform rt = (RectTransform)mainScrollContentView.transform;
-            for (int i = 0; i < tutorialList.Count; i++)
-            {
-                string value = tutorialList [i];
-                string studentNumber = studentNumberList [i];
-                GameObject playerTextPanel = (GameObject)Instantiate(ContentDataPanel);
-                playerTextPanel.transform.SetParent(mainScrollContentView.transform);
-                playerTextPanel.transform.localScale = new Vector3(1,1,1);
-                playerTextPanel.transform.localPosition = new Vector3(0,0,0);
-                playerTextPanel.transform.Find ("Cell_Text").GetComponent<Text> ().text=i + ".                  " + value + "                  " + studentNumber + "                  " + "Manage";
-            }
-        }
-        else 
-        {
-            noRecordLabel.gameObject.SetActive (true);
-        }
-    }
-
-    public void hideTutorialListPopUp() 
-    {
-        tutorialPopUp.gameObject.SetActive (false);
-    }
-
-    public void showTutorialListPopUp() 
-    {
-        tutorialPopUp.gameObject.SetActive (true);
-    }
-
-
-
-}
-*/
+using UnityEngine.Networking;
+using SimpleJSON;
+using System.Linq;
 
 
 public class Tutorial_List_Script : MonoBehaviour
 {
+    
     List<string> tutorialList;
-    List<string> studentNumberList;
+    List<JSONNode> studentNumberList;
+    //List<string> studentNumberList;
     GameObject tutorialPopUp;
     public Text noRecordLabel;
     public GameObject mainScrollContentView;
     public GameObject ContentDataPanel;
     //public Tutorial_Group_API_Controller tutorialAPI;
+    List<string> indexNumberTextArray = new List<string>();
+    List<JSONNode> allStudentsArray = new List<JSONNode>();
+    
 
+    private readonly string baseTutorialIndexURL = "https://seheroes.herokuapp.com/tutorialGroup";
+    
     //use this for initialization
     void Start () 
-    {
-       setTutorialListData ();
-       setStudentsNumberListData();
-       tutorialPopupInitialized ();
+    { 
+       StartCoroutine(GetTutorialIndex());
+       
     }
 
-    //Update is called once per frame
-    void Update ()
+    IEnumerator GetTutorialIndex()
     {
+        UnityWebRequest tutorialIndexRequest = UnityWebRequest.Get(baseTutorialIndexURL);
 
-    }
+        yield return tutorialIndexRequest.SendWebRequest();
 
-    //public void backButtonAction
-    //{
-
-    //}
-
-    //public void computerButtonAction
-    //{
-        //manager.Instance.isComputerVsPlayer = true;
-        //SceneManager.LoadScene (2);
-    //}
-
-
-    //Tutorial popup methods
-    void setTutorialListData() 
-    {
-        tutorialList = new List<string> ();
-        //tutorialList = tutorialAPI.GetTutorialIndexArray();
-        int index;
-        for (int i = 0; i < 15; i++)
+        if (tutorialIndexRequest.isNetworkError || tutorialIndexRequest.isHttpError)
         {
-            index = Random.Range(0, 10000);
-            tutorialList.Add (index.ToString());
+            //Debug.LogError(tutorialIndexRequest.error);
+            yield break;
         }
-    }
 
-    void setStudentsNumberListData() 
-    {
-        studentNumberList = new List<string> ();
-        int index;
-        for (int i = 0; i < 15; i++)
+        JSONNode tutorialInfo = JSON.Parse(tutorialIndexRequest.downloadHandler.text);
+        
+        for (int i = 0; i < tutorialInfo.Count; i++)
         {
-            index = Random.Range(0, 100);
-            studentNumberList.Add (index.ToString());
+            indexNumberTextArray.Add(tutorialInfo[i]["tutorialGroupId"]);
+            allStudentsArray.Add(tutorialInfo[i]["student"]);
+            //Debug.Log();
+            
         }
-    }
 
-    void tutorialPopupInitialized () 
-    {
+        tutorialList = indexNumberTextArray;
+        
+
+        studentNumberList = new List<JSONNode>();
+        studentNumberList = allStudentsArray;
+        Debug.Log(studentNumberList);
+        
         if (tutorialList.Count > 0)
         {
-            noRecordLabel.gameObject.SetActive (false);
+            noRecordLabel.gameObject.SetActive(false);
             RectTransform rt = (RectTransform)mainScrollContentView.transform;
             for (int i = 0; i < tutorialList.Count; i++)
-            {
-                string value = tutorialList [i];
-                string studentNumber = studentNumberList [i];
+            { 
+                string value = tutorialList[i];
+                string studentNumber = studentNumberList[i].Count.ToString();
+                Debug.Log(studentNumberList[i].Count);
+                //string studentNumber = studentNumberList[i];
                 GameObject playerTextPanel = (GameObject)Instantiate(ContentDataPanel);
                 playerTextPanel.transform.SetParent(mainScrollContentView.transform);
                 playerTextPanel.transform.localScale = new Vector3(1,1,1);
@@ -184,6 +82,8 @@ public class Tutorial_List_Script : MonoBehaviour
         {
             noRecordLabel.gameObject.SetActive (true);
         }
+        
+
     }
 
     public void hideTutorialListPopUp() 
