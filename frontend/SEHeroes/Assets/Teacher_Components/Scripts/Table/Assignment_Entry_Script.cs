5084cead734090e4ad788190b5bfe0c70b9962f9
oongjiexiang <oongjiexiang@yahoo.com>
2021-04-03 21:52:05 +0800

    Assignment Metadata page done

--- frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Assignment_Entry_Script.cs
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Assignment_Entry_Script.cs
@@ -13,7 +13,7 @@ public class Assignment_Entry_Script : MonoBehaviour
     private Transform entryTemplate;
     private Transform entryContainer;
     private GameObject popUp;
-    List<assignment> assignmentList;
+    List<Assignment> assignmentList;
 
     public Text noRecordLabel;
     public GameObject mainContentPanel;
@@ -23,104 +23,32 @@ public class Assignment_Entry_Script : MonoBehaviour
 
     IEnumerator Start()
     {
-        yield return StartCoroutine(setAssignmentList());
         popUp = mainContentPanel.transform.Find("Panel_Messages").gameObject;
         popUp.SetActive(false);
+        yield return StartCoroutine(setAssignmentList());
         tableInitialize();
-    }
-    private assignment dummyCreateAssign(){
-        assignment asg = new assignment();
-        asg.assignmentName = "assg1a";
-        asg.tries = 2;
-        asg.startDate = 1124;      // will change
-        asg.dueDate = 21351325;        // will change
-        // asg.questions = new List<assignmentQuestion>();
-        asg.questions = new List<string>();
-        asg.questions.Add("What is my course?");
-        asg.questions.Add("What is your year?");
-        asg.questions.Add("What is his school?");
-        // assignmentQuestion q1 = new assignmentQuestion();
-        // q1.question = "What is your name?";
-        // q1.score = 100;
-        // q1.answer = new List<string>();
-        // string[] temp = { "Ash", "Bob", "chakra", "Dan" };
-        // q1.answer.AddRange(temp);
-        // q1.correctAnswer = q1.answer[2];
-        // q1.image = "";
-        // asg.questions.Add(q1);
-
-        // assignmentQuestion q2 = new assignmentQuestion();
-        // q2.question = "What is your age?";
-        // q2.score = 100;
-        // q2.answer = new List<string>();
-        // string[] temp2 = { "19", "29", "39", "49" };
-        // q2.answer.AddRange(temp2);
-        // q2.correctAnswer = q2.answer[1];
-        // q2.image = "";
-        // asg.questions.Add(q2);
-
-        // assignmentQuestion q3 = new assignmentQuestion();
-        // q3.question = "What is your age?";
-        // q3.score = 100;
-        // q3.answer = new List<string>();
-        // string[] temp3 = {"19", "29", "39", "49"};
-        // q3.answer.AddRange(temp3);
-        // q3.correctAnswer = q3.answer[3];
-        // q3.image = "";
-        // asg.questions.Add(q3);
-        return asg; 
-    }
-    IEnumerator dummyPostAssignment(assignment asg){
-        API_Connection conn = new API_Connection();
-        string jsonToPost = JsonUtility.ToJson(asg);
-        print(jsonToPost + " is post url for assignment");
-        yield return conn.PostData("assignment", jsonToPost);
-    }
-    IEnumerator dummyPutAssignment(assignment asg){
-        API_Connection conn = new API_Connection();
-        string jsonToPut = JsonUtility.ToJson(asg);
-        print(jsonToPut + " is PUT url for assignment");
-        yield return conn.PutData("assignment/1rO3ShtqbePhkjoQ2Jcd", jsonToPut);
-    }
-    IEnumerator dummyDeleteAssignment(assignment asg){
-        API_Connection conn = new API_Connection();
-        yield return conn.DeleteData("assignment/1rO3ShtqbePhkjoQ2Jcd");
     }
     IEnumerator setAssignmentList()
     {
         API_Connection conn = new API_Connection();
         JSONNode jsonNode = null;
         // print("in main: " + json_receivedData.Count);
-        yield return StartCoroutine(conn.GetData("assignment", null, s => {
+        yield return StartCoroutine(conn.GetData("Assignment", null, s => {
             jsonNode = JSON.Parse(s);
         }));
-        assignmentList = new List<assignment>();
+        assignmentList = new List<Assignment>();
         for(int i = 0; i < jsonNode.Count; i++){
-            assignment asg = new assignment();
+            Assignment asg = new Assignment();
             asg.assignmentName = jsonNode[i]["assignmentName"];
             asg.tries = jsonNode[i]["tries"];
-            asg.startDate = jsonNode[i]["startDate"];
-            asg.dueDate = jsonNode[i]["dueDate"];
+            asg.startDate = DateTime.ParseExact(jsonNode[i]["startDate"], "yyyy/MM/dd HH:mm", null);
+            asg.dueDate = DateTime.ParseExact(jsonNode[i]["dueDate"], "yyyy/MM/dd HH:mm", null);
             asg.questions = new List<string>();
             for(int j = 0; j < jsonNode[i]["questions"].Count; j++)
                 asg.questions.Add(jsonNode[i]["questions"][j]);
             assignmentList.Add(asg);
         }
-        // assignmentNames = new List<string>();
-        // assignmentStatus = new List<string>();
-        // var list = new List<string> { "Ongoing", "Closed", "Scheduled" };
-
-        // int week = 0;
-        // var rd = new System.Random();
-        // for (int i = 0; i < 20; i++)
-        // {
-        //     week++;
-        //     assignmentNames.Add("Week " + week.ToString());
-        //     int index = rd.Next(list.Count);
-        //     assignmentStatus.Add(list[index]);
-        // }
     }
-
     void tableInitialize()
     {
         entryContainer = content.transform.Find("Assignment_Entry_Container");
@@ -135,10 +63,12 @@ public class Assignment_Entry_Script : MonoBehaviour
             entryTransform.Find("Text_No").GetComponent<Text>().text = i.ToString();
             entryTransform.Find("Text_Name").GetComponent<Text>().text = assignmentList[i].assignmentName;
             entryTransform.Find("Text_Status").GetComponent<Text>().text = assignmentList[i].dueDate.ToString();
+            entryTransform.Find("Text_Name").Find("Button_Edit").Find("Text").GetComponent<Text>().text = "View";
             entryTransform.Find("Text_Name").Find("Button_Edit").GetComponent<Button>().onClick.AddListener(() => {
                 assignmentName = entryTransform.Find("Text_Name").GetComponent<Text>().text;
                 viewAssignment();
             });
+            entryTransform.Find("Text_Name").Find("Button_Delete").Find("Text").GetComponent<Text>().text = "Delete";
             entryTransform.Find("Text_Name").Find("Button_Delete").GetComponent<Button>().onClick.AddListener(() => {
                 assignmentName = entryTransform.Find("Text_Name").GetComponent<Text>().text;
                 viewAssignment();
---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Create_Assignment_Meta_Script.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Create_Assignment_Meta_Script : MonoBehaviour
+{
+    const int NUM_TRY = 10;
+    private Assignment newAsg;
+    public GameObject panelObject;
+    public GameObject mainContentPanel;
+    private GameObject popUp;
+    void Start()
+    {
+        newAsg = new Assignment();
+        popUp = mainContentPanel.transform.Find("Panel_Messages").gameObject;
+        popUp.SetActive(false);
+        populateRetries();
+        panelObject.transform.Find("Button_Cancel").GetComponent<Button>().onClick.AddListener(() => clickCancel());
+        panelObject.transform.Find("Button_Create").GetComponent<Button>().onClick.AddListener(() => clickCreate());
+        mainContentPanel.transform.Find("Panel_Messages").Find("Popup_Incomplete").Find("Button_Confirm").GetComponent<Button>().onClick.AddListener(() => incompleteAcknowledge());
+    }
+    void populateRetries(){
+        List<string> numTry = new List<string>();
+        for(int i = 1; i < NUM_TRY; i++){
+            numTry.Add(i.ToString());
+        }
+        panelObject.transform.Find("Dropdown_Try").GetComponent<Dropdown>().AddOptions(numTry);
+    }
+    // event handlers
+    public void clickCancel(){
+        SceneManager.LoadScene("Assignments");
+    }
+    public void clickCreate(){
+        if(verifyFields()) SceneManager.LoadScene("Assignments_Creation");
+        else{
+            popUp.SetActive(true);
+            popUp.transform.Find("Popup_Incomplete").gameObject.SetActive(true);
+        }
+    }
+    public void incompleteAcknowledge(){
+        popUp.SetActive(false);
+    }
+    private void incompletePopup(string message){
+        popUp.transform.Find("Popup_Incomplete").Find("Text").GetComponent<Text>().text = message;
+    }
+    // logic
+    private Dropdown getDropdown(string dropdownSection, string dropdownName){
+        return panelObject.transform.Find(dropdownSection).Find(dropdownName).GetComponent<Dropdown>();
+    }
+    public bool verifyFields(){
+        List<Dropdown> dropdowns = new List<Dropdown>();
+        List<int> time = new List<int>();
+
+        Dropdown dropdown = panelObject.transform.Find("Dropdown_Try").GetComponent<Dropdown>();
+        Dropdown startMonthD = getDropdown("Handle_Start_Date", "Dropdown_Month");
+        Dropdown endMonthD = getDropdown("Handle_End_Date", "Dropdown_Month");
+        newAsg.assignmentName = panelObject.transform.Find("InputField_Name").Find("Text").GetComponent<Text>().text;
+        newAsg.tries = int.Parse(dropdown.options[dropdown.value].text);
+        string startMonth = startMonthD.options[startMonthD.value].text;
+        string endMonth = endMonthD.options[endMonthD.value].text;
+
+        dropdowns.Add(getDropdown("Handle_Start_Date", "Dropdown_Year"));
+        dropdowns.Add(getDropdown("Handle_Start_Date", "Dropdown_Day"));
+        dropdowns.Add(getDropdown("Handle_Start_Time", "Dropdown_Hour"));
+        dropdowns.Add(getDropdown("Handle_Start_Time", "Dropdown_Minute"));
+        dropdowns.Add(getDropdown("Handle_End_Date", "Dropdown_Year"));
+        dropdowns.Add(getDropdown("Handle_End_Date", "Dropdown_Day"));
+        dropdowns.Add(getDropdown("Handle_End_Time", "Dropdown_Hour"));
+        dropdowns.Add(getDropdown("Handle_End_Time", "Dropdown_Minute"));
+        foreach(var dp in dropdowns){
+            time.Add(int.Parse(dp.options[dp.value].text));
+        }
+        
+        newAsg.startDate = DateTime.Parse(String.Format("{0} {1} {2} {3}:{4}:{5}", time[0], startMonth, time[1], time[2], time[3], 0));
+        newAsg.dueDate = DateTime.Parse(String.Format("{0} {1} {2} {3}:{4}:{5}", time[4], endMonth, time[5], time[6], time[7], 0));
+        if(newAsg.assignmentName == ""){
+            incompletePopup("Please give the assignment a name.");
+            return false;
+        }
+        if(newAsg.startDate <= DateTime.Now){
+            incompletePopup("Start date has passed. Please choose another date");
+            return false;
+        }
+        if(newAsg.startDate >= newAsg.dueDate){
+            incompletePopup("Start date cannot be later than the due date");
+            return false;
+        }
+        return true;
+    }
+}
