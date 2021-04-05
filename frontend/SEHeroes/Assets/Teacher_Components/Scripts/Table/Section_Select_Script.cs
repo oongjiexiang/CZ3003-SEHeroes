305b8ab8693f4ad752032b8ac52c6f02a9992988
Bryan Lim <blim0901@gmail.com>
2021-04-05 09:05:06 +0800

    Created scenes for leaderboard and question bank editor. Also made changes to question bank UI scenes

---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/Section_Select_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using SimpleJSON;
+using System.Linq;
+
+public class Section_Select_Script : MonoBehaviour
+{
+
+    public string sectionChoice;
+    public GameObject inputField;
+
+    // Start is called before the first frame update
+    public void onSectionSelect()
+    {
+        sectionChoice = inputField.GetComponent<Text>().text;
+        SceneManager.LoadScene("Question_Bank_Management");
+    }
+ 
+
+}
