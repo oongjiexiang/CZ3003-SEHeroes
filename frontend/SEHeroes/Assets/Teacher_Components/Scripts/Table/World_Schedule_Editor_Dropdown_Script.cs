8203fb4b467d17390f0b83b73a65267a4ebdcbea
Bryan Lim <blim0901@gmail.com>
2021-04-11 14:25:03 +0800

    added scenes for world scheduling and world schedule editor

---
+++ frontend/SEHeroes/Assets/Teacher_Components/Scripts/Table/World_Schedule_Editor_Dropdown_Script.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using SimpleJSON;
+using System.Linq;
+
+public class World_Schedule_Editor_Dropdown_Script : MonoBehaviour
+{
+
+    public static string sectionChoice;
+
+    void Start()
+    {
+        var dropdown = transform.GetComponent<Dropdown>();
+
+        dropdown.options.Clear();
+
+        List<string> items = new List<string>();
+
+        if (World_Select_Editor_Script.worldChoice == "Planning")
+        {
+            items.Add("Introduction");
+            items.Add("Requirement Elicitation");
+            items.Add("Requirement Analysis");
+            items.Add("Analysis Model");
+            items.Add("Documentation(SRS)");
+        }
+
+        foreach (var item in items)
+        {
+            dropdown.options.Add(new Dropdown.OptionData() { text = item });
+        }
+
+        DropdownItemSelected(dropdown);
+
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+
+    }
+
+    public void DropdownItemSelected(Dropdown dropdown)
+    {
+        int index = dropdown.value;
+        try{
+            sectionChoice = dropdown.options[index].text;
+        }
+        catch(ArgumentOutOfRangeException){
+            sectionChoice = "";
+        }
+    }
+
+    // Start is called before the first frame update
+    public void onSectionSelect()
+    {
+        SceneManager.LoadScene("Question_Bank_Management");
+    }
+ 
+
+}
